364a11a4c580ed448927d42a936279873c2c4132
tao <reaidea@qq.com>
2026-02-27 18:44:30 +0800

    fix: 同步根目录的articles.js

--- articles.js
+++ articles.js
@@ -1,3 +1,4 @@
 const articles = [
-  { file: 'bio/da-yu.md', title: '大禹：治理洪水的人', period: '约前2200年，华夏文明的第一个转折点' }
+  { file: 'docs/bio/da-yu.md', title: '大禹：治理洪水的人', period: '约前2200年，华夏文明的第一个转折点' },
+  { file: 'docs/bio/kongzi.md', title: '孔子：失败者与塑造者', period: '' }
 ];
