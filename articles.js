3c8fed25107562e09190f8d7dc25e5ed5e652904
tao <reaidea@qq.com>
2026-02-27 21:27:37 +0800

    更新索引：添加墨子

--- articles.js
+++ articles.js
@@ -1,5 +1,6 @@
 const articles = [
   { file: 'docs/bio/da-yu.md', title: '大禹：治理洪水的人', period: '约前2200年，华夏文明的第一个转折点' },
   { file: 'docs/bio/kongzi.md', title: '孔子：失败者与塑造者', period: '' },
-  { file: 'docs/bio/laozi.md', title: '老子：沉默的书写者', period: '"老子者，楚苦县厉乡曲仁里人也，姓李氏，名耳，字聃，周守藏室之史也。"' }
+  { file: 'docs/bio/laozi.md', title: '老子：沉默的书写者', period: '"老子者，楚苦县厉乡曲仁里人也，姓李氏，名耳，字聃，周守藏室之史也。"' },
+  { file: 'docs/bio/mozhi.md', title: '墨子：孤独的和平主义者与手工业者的哲学', period: '"兼爱，非攻。" —— 墨子' }
 ];
