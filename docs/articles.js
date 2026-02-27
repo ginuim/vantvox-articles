25e70ef2aa53d5909099f3fdfb467948c5461289
tao <reaidea@qq.com>
2026-02-27 18:56:10 +0800

    Add: 老子传记

--- docs/articles.js
+++ docs/articles.js
@@ -1,4 +1,5 @@
 const articles = [
   { file: 'docs/bio/da-yu.md', title: '大禹：治理洪水的人', period: '约前2200年，华夏文明的第一个转折点' },
-  { file: 'docs/bio/kongzi.md', title: '孔子：失败者与塑造者', period: '' }
+  { file: 'docs/bio/kongzi.md', title: '孔子：失败者与塑造者', period: '' },
+  { file: 'docs/bio/laozi.md', title: '老子：沉默的书写者', period: '"老子者，楚苦县厉乡曲仁里人也，姓李氏，名耳，字聃，周守藏室之史也。"' }
 ];
