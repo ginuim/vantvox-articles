b4afc0b0befc4bb0b16ea0e7cf5a7a19b012cff8
tao <reaidea@qq.com>
2026-02-27 22:47:48 +0800

    更新索引

--- articles.js
+++ articles.js
@@ -2,5 +2,5 @@ const articles = [
   { file: 'docs/bio/da-yu.md', title: '大禹：治理洪水的人', period: '约前2200年，华夏文明的第一个转折点' },
   { file: 'docs/bio/kongzi.md', title: '孔子：失败者与塑造者', period: '' },
   { file: 'docs/bio/laozi.md', title: '老子：沉默的书写者', period: '"老子者，楚苦县厉乡曲仁里人也，姓李氏，名耳，字聃，周守藏室之史也。"' },
-  { file: 'docs/bio/mozhi.md', title: '墨子：孤独的和平主义者与手工业者的哲学', period: '"兼爱，非攻。" —— 墨子' }
+  { file: 'docs/bio/mozhi.md', title: '墨子：中国第一个有组织的理想主义者', period: '公元前440年，一个穿着草鞋的木匠，用一根小木签化解了当时最先进的所有攻城器械。他不是来打架的，他是来阻止战争的。' }
 ];
